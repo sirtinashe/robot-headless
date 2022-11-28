c9c39124c104b4d21cec35e386ce57d2ff9addf9
TinasheMashaya <tinashemashaya18@gmail.com>
2022-11-28 17:19:09 +0200

    updated mt4

---
+++ mt4/MQL4/Scripts/sample1.py
@@ -0,0 +1,15 @@
+# Copyright 2022, MetaQuotes Software Corp.
+# https://www.mql5.com
+
+from datetime import datetime
+import MetaTrader5 as mt5
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+mt5.initialize()
+
+# you code here
+# 
+
+mt5.shutdown()
---
+++ mt4/profiles/default/start.tpl
@@ -0,0 +1,5 @@
+<chart>
+symbol=GBPJPY
+temp_chart=1
+period=60
+</chart>
